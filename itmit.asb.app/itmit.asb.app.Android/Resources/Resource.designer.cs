8f9d5e9a48602702547dfa85d603124c2d8bc928
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-07-08 16:14:24 +0300

    Меню и картинки

--- itmit.asb.app/itmit.asb.app.Android/Resources/Resource.designer.cs
+++ itmit.asb.app/itmit.asb.app.Android/Resources/Resource.designer.cs
@@ -2342,26 +2342,26 @@ namespace itmit.asb.app.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020140
-			public const int avd_hide_password_1 = 2130837824;
-			
 			// aapt resource value: 0x7f020141
-			public const int avd_hide_password_2 = 2130837825;
+			public const int avd_hide_password_1 = 2130837825;
 			
 			// aapt resource value: 0x7f020142
-			public const int avd_hide_password_3 = 2130837826;
+			public const int avd_hide_password_2 = 2130837826;
+			
+			// aapt resource value: 0x7f020143
+			public const int avd_hide_password_3 = 2130837827;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020143
-			public const int avd_show_password_1 = 2130837827;
-			
 			// aapt resource value: 0x7f020144
-			public const int avd_show_password_2 = 2130837828;
+			public const int avd_show_password_1 = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int avd_show_password_3 = 2130837829;
+			public const int avd_show_password_2 = 2130837829;
+			
+			// aapt resource value: 0x7f020146
+			public const int avd_show_password_3 = 2130837830;
 			
 			// aapt resource value: 0x7f020055
 			public const int border_alarm = 2130837589;
@@ -3041,11 +3041,11 @@ namespace itmit.asb.app.Droid
 			// aapt resource value: 0x7f020136
 			public const int notification_icon_background = 2130837814;
 			
-			// aapt resource value: 0x7f02013e
-			public const int notification_template_icon_bg = 2130837822;
-			
 			// aapt resource value: 0x7f02013f
-			public const int notification_template_icon_low_bg = 2130837823;
+			public const int notification_template_icon_bg = 2130837823;
+			
+			// aapt resource value: 0x7f020140
+			public const int notification_template_icon_low_bg = 2130837824;
 			
 			// aapt resource value: 0x7f020137
 			public const int notification_tile_bg = 2130837815;
@@ -3063,10 +3063,13 @@ namespace itmit.asb.app.Droid
 			public const int tooltip_frame_light = 2130837819;
 			
 			// aapt resource value: 0x7f02013c
-			public const int user = 2130837820;
+			public const int user1 = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
 			public const int xamarin_logo = 2130837821;
+			
+			// aapt resource value: 0x7f02013e
+			public const int zvonok = 2130837822;
 			
 			static Drawable()
 			{
