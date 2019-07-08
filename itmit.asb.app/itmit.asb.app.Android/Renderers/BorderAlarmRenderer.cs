54149512924af6d8c32e95d9ba12869a999fc827
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-07-08 11:25:18 +0300

    first commit

---
+++ itmit.asb.app/itmit.asb.app.Android/Renderers/BorderAlarmRenderer.cs
@@ -0,0 +1,17 @@
+﻿using Android.Content;
+using itmit.asb.app.Controls;
+using itmit.asb.app.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderAlarm), typeof(BorderAlarmRenderer))]
+namespace itmit.asb.app.Droid.Renderers
+{
+    public class BorderAlarmRenderer : VisualElementRenderer<Frame>
+    {
+        public BorderAlarmRenderer(Context context) : base(context)
+        {
+            SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.border_alarm));
+        }
+    }
+}
