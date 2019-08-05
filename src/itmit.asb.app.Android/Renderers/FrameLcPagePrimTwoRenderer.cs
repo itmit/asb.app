5a45e2d63b9092f7762800c0ee35881424f0eb86
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-05 17:59:21 +0300

    Изменение личного кабинета

---
+++ src/itmit.asb.app.Android/Renderers/FrameLcPagePrimTwoRenderer.cs
@@ -0,0 +1,18 @@
+﻿using Android.Content;
+using itmit.asb.app.Controls;
+using itmit.asb.app.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(PrimLcPageTwo), typeof(FrameLcPagePrimTwoRenderer))]
+
+namespace itmit.asb.app.Droid.Renderers
+{
+    public class FrameLcPagePrimTwoRenderer : VisualElementRenderer<Frame>
+    {
+        public FrameLcPagePrimTwoRenderer(Context context) : base(context)
+        {
+            SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.Prim2));
+        }
+    }
+}
