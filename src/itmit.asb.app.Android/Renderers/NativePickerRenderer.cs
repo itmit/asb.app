bda069e77631ae8ddad6d4b73e5ef33cd25c623f
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-09-12 15:51:57 +0300

    Внешние правки

---
+++ src/itmit.asb.app.Android/Renderers/NativePickerRenderer.cs
@@ -0,0 +1,35 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using itmit.asb.app.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Android.Graphics.Color;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(NativePickerRenderer))]
+namespace itmit.asb.app.Droid.Renderers
+{
+    public class NativePickerRenderer : PickerRenderer
+    {
+        public NativePickerRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                {
+                    Control.BackgroundTintList = ColorStateList.ValueOf(Color.Rgb(199, 11, 9));
+                }
+                else
+                {
+                    Control.Background.SetColorFilter(Color.Rgb(199, 11, 9), PorterDuff.Mode.SrcAtop);
+                }
+            }
+        }
+    }
+}
